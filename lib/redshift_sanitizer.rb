2ce9a8b40a78a101192a7bb4eed4a366989440ba
BranLiang <lby89757@hotmail.com>
2019-11-02 15:09:39 +0800

    Update gemspec

---
+++ lib/redshift_sanitizer.rb
@@ -0,0 +1,6 @@
+require "redshift_sanitizer/version"
+
+module RedshiftSanitizer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/redshift_sanitizer/version.rb
@@ -0,0 +1,3 @@
+module RedshiftSanitizer
+  VERSION = "0.1.0"
+end
